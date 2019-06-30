2fe1e730861f87a061de4ef071ede20bbb3e9e90
fanguyun214 <289696260@qq.com>
2019-06-30 22:45:03 +0800

    :construction: (pm2)

--- app.js
+++ app.js
@@ -17,6 +17,22 @@ router.get('/', async ctx => {
   };
 });
 
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
 // 引入路由
 const users = require('./routes/api/users');
 
